b0daa80c9794653393ceee13237f8bbde9bf2c17
Tiago <tiagoletras45@gmail.com>
2020-01-08 16:31:08 -0300

    Add webpack config files.

---
+++ webpack.common.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: Configuration = {
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'source-map-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      title: 'Telzir'
+    })
+  ]
+};
+
+export default config;
