829c1226514faca206e8333110db28a25c8771a2
Tiago <tiagoletras45@gmail.com>
2020-01-08 07:12:20 -0300

    Update getMinutePrice and create default export.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,50 @@
-export const getMinutePrice = ddd => {
-  if (ddd === 1116) return 1.9;
-  if (ddd === 1611) return 2.9;
-  if (ddd === 1117) return 1.7;
-  if (ddd === 1711) return 2.7;
-  if (ddd === 1118) return 0.9;
-  if (ddd === 1811) return 1.9;
+/**
+ * @param {number} from
+ * @param {number} to
+ * @param {number} minutes
+ *
+ * @returns {number[]} total - is an array of 4 values:
+ * total[0] = price withou FaleMais
+ * total[1] = price with FaleMais 30 minutos
+ * total[2] = price with FaleMais 60 minutos
+ * total[3] = price with FaleMais 120 minutos
+ */
+export default (
+  from: 11 | 16 | 17 | 18,
+  to: 11 | 16 | 17 | 18,
+  minutes: number
+) => {
+  const minutePrice = getMinutePrice(from, to);
+  const plans = [0, 30, 60, 120];
+
+  if (minutePrice === 0) {
+    throw new Error('DDD inválido');
+
+    return null;
+  }
+
+  let total: number[] = [];
+
+  total = plans.map(plan => {
+    return getTotal(minutePrice, minutes, plan);
+  });
+
+  return total;
+};
+
+/**
+ * @param {number} from
+ * @param {number} to
+ *
+ * @returns {number} - minute cost. if it returns 0, at least one param is invalid.
+ */
+export const getMinutePrice = (ddd1: number, ddd2: number) => {
+  if (ddd1 === 11 && ddd2 === 16) return 1.9;
+  if (ddd1 === 16 && ddd2 === 11) return 2.9;
+  if (ddd1 === 11 && ddd2 === 17) return 1.7;
+  if (ddd1 === 17 && ddd2 === 11) return 2.7;
+  if (ddd1 === 11 && ddd2 === 18) return 0.9;
+  if (ddd1 === 18 && ddd2 === 11) return 1.9;
   return 0;
 };
 
@@ -16,21 +56,6 @@ export const getTotal = (
   let price: number;
 
   price = plan ? (minutes - plan) * minutePrice : minutes * minutePrice;
-  return plan > 0 ? plan : 0;
-};
 
-export const priceWithPlan = (
-  pricePerMinute: number,
-  minutes: number,
-  plan: number
-) => {
-  pricePerMinute += pricePerMinute * 0.1;
-  const price = (minutes - plan) * pricePerMinute;
-  if (price < 0) return 0;
-  return price;
-};
-
-export const priceWithOutPlan = (pricePerMinute: number, minutes: number) => {
-  const price = minutes * pricePerMinute;
-  return price;
+  return price > 0 ? price : 0;
 };
