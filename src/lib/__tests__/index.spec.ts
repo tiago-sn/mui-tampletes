28b5c5e516eafd0e6fccbd94e13ee36bea37c4b9
Tiago <tiagoletras45@gmail.com>
2020-01-08 09:05:16 -0300

    Add tests: getMinutePrice new public api.

--- src/lib/__tests__/index.spec.ts
+++ src/lib/__tests__/index.spec.ts
@@ -1,4 +1,5 @@
 import { getMinutePrice, getTotal } from '../index';
+import { priceTable } from '../priceTable';
 
 describe('src/lib/index/getMinutePrice()', () => {
   const ddd1 = 11,
@@ -7,27 +8,27 @@ describe('src/lib/index/getMinutePrice()', () => {
     ddd4 = 18;
 
   it(`from ${ddd1} to ${ddd2}`, () => {
-    expect(getMinutePrice(ddd1, ddd2)).toBe(1.9);
+    expect(getMinutePrice(ddd1, ddd2, priceTable)).toBe(1.9);
   });
 
   it(`from ${ddd2} to ${ddd1}`, () => {
-    expect(getMinutePrice(ddd2, ddd1)).toBe(2.9);
+    expect(getMinutePrice(ddd2, ddd1, priceTable)).toBe(2.9);
   });
 
   it(`from ${ddd1} to ${ddd3}`, () => {
-    expect(getMinutePrice(ddd1, ddd3)).toBe(1.7);
+    expect(getMinutePrice(ddd1, ddd3, priceTable)).toBe(1.7);
   });
 
   it(`from ${ddd3} to ${ddd1}`, () => {
-    expect(getMinutePrice(ddd3, ddd1)).toBe(2.7);
+    expect(getMinutePrice(ddd3, ddd1, priceTable)).toBe(2.7);
   });
 
   it(`from ${ddd1} to ${ddd4}`, () => {
-    expect(getMinutePrice(ddd1, ddd4)).toBe(0.9);
+    expect(getMinutePrice(ddd1, ddd4, priceTable)).toBe(0.9);
   });
 
   it(`from ${ddd4} to ${ddd1}`, () => {
-    expect(getMinutePrice(ddd4, ddd1)).toBe(1.9);
+    expect(getMinutePrice(ddd4, ddd1, priceTable)).toBe(1.9);
   });
 });
 
