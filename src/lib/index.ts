8bd8210295cbfc884aea21b8ad2f0796a67e6efd
Tiago <tiagoletras45@gmail.com>
2020-01-08 09:08:27 -0300

    Delete DEPRECATED function.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -54,16 +54,6 @@ export const getMinutePrice = (
   return price[0].value;
 };
 
-export const getMinutePriceDEPRECATED = (ddd1: number, ddd2: number) => {
-  if (ddd1 === 11 && ddd2 === 16) return 1.9;
-  if (ddd1 === 16 && ddd2 === 11) return 2.9;
-  if (ddd1 === 11 && ddd2 === 17) return 1.7;
-  if (ddd1 === 17 && ddd2 === 11) return 2.7;
-  if (ddd1 === 11 && ddd2 === 18) return 0.9;
-  if (ddd1 === 18 && ddd2 === 11) return 1.9;
-  return 0;
-};
-
 export const getTotal = (
   minutePrice: number,
   minutes: number,
