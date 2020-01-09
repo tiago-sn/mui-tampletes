6560d5da457ace624ad57e3f6faac5652478e668
Tiago <tiagoletras45@gmail.com>
2020-01-09 14:23:55 -0300

    Add <SimulationPanel /> to <App />

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { ChangeEvent, useReducer } from 'react';
+import SimulationPanel from './containers/SimulationPanel';
 
-const App: React.FC = () => <h1>Telzir</h1>;
+interface State {
+  from: number;
+  to: number;
+}
+
+type Action = { type: 'from'; value: number } | { type: 'to'; value: number };
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'from':
+      return { ...state, from: action.value };
+    case 'to':
+      return { ...state, to: action.value };
+    default:
+      return state;
+  }
+};
+
+const initialState: State = {
+  from: 11,
+  to: 16
+};
+
+const App: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const selectDDD = (
+    e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const value = parseInt(e.target.value);
+    const { name } = e.target;
+    dispatch({ type: name as 'to' | 'from', value });
+  };
+
+  return <SimulationPanel onSelectDDD={selectDDD} />;
+};
 
 export default App;
