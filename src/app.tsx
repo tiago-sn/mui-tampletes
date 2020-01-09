35599370122fea84fba6753577fac8b529727e30
Tiago <tiagoletras45@gmail.com>
2020-01-09 17:57:22 -0300

    <App />: add simulation feature.

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import React, { useReducer } from 'react';
+import simulate from './lib';
 import SimulationPanel from './components/SimulationPanel';
-import { ReactChangeEvent, State, Action } from './app.types';
+import { ReactChangeEvent, State, ReactMouseEvent, DDD } from './app.types';
 import reducer from './reducer';
 
 const initialState: State = {
   from: 11,
   to: 16,
-  minutes: 0
+  minutes: 0,
+  simulations: []
 };
 
 const App: React.FC = () => {
@@ -18,7 +20,13 @@ const App: React.FC = () => {
     dispatch({ type: name as 'to' | 'from' | 'minutes', value });
   };
 
-  return <SimulationPanel onInputChange={selectDDD} />;
+  const onSimulate = (e: ReactMouseEvent) => {
+    const { from, to, minutes } = state;
+    const simulations = simulate(from as DDD, to as DDD, minutes as DDD);
+    dispatch({ type: 'addSimulation', value: simulations });
+  };
+
+  return <SimulationPanel onInputChange={selectDDD} onSimulate={onSimulate} />;
 };
 
 export default App;
