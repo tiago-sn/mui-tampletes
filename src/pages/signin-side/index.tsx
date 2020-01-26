3c89fe761a4327f43c013f95e43089212142bca0
Tiago Santo <tiagosn03@gmail.com>
2020-01-25 23:03:34 -0300

    Extract styles from component.

--- src/pages/signin-side/index.tsx
+++ src/pages/signin-side/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import { LockOutlined } from '@material-ui/icons';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+  CssBaseline
+} from '@material-ui/core';
+
+import useStyles from './styles';
 
 function Copyright(): JSX.Element {
   return (
@@ -25,39 +28,6 @@ function Copyright(): JSX.Element {
     </Typography>
   );
 }
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh'
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark'
-        ? theme.palette.grey[900]
-        : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
 
 export default function SignInSide(): JSX.Element {
   const classes = useStyles();
